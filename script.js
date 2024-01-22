dfbfcf3578bc376bc688e774942dc1b0eb4e7349
Bhupesh Dahiya <149587292+bhupesh56@users.noreply.github.com>
2024-01-22 12:09:21 +0530

    Implement 'operate' function and refactor variable
    Introduced a new 'operate' function in script.js for enhanced
    functionality and conducted variable name refactoring for improved code
    clarity.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let buttons = Array.from(document.querySelectorAll("button"));
 let output = document.querySelector(".screen");
-let displayValue = result = 0;
-let num1 = num2 = "";
+let math = ["/", "+", "-", "*"];
+let numArr = [];
 let operator = "";
-let flag = 0;
+let i = 0;
 
 buttons.forEach(button => {
     button.addEventListener('click', function(event) {
@@ -12,84 +12,107 @@ buttons.forEach(button => {
   });
 
 function handleButtonClick(buttonId) {
-  let element = document.querySelector(`#${buttonId}`);
-  let numValue = element.textContent;
-  populateDisplay(element.textContent);
+  let text = document.querySelector(`#${buttonId}`).textContent;
   switch (buttonId) {
     case "del":
-      if(flag === 0){
-        num1 = num1.slice(0, -1);
-      }
-      else{
-        num2 = num2.slice(0, -1);
-      }
+      numArr.pop();
+      i--;
       break;
     case "ac":
-      num1 = num2 = flag = 0;
-      break;
-    case "one":
-    case "two":
-    case "three":
-    case "four":
-    case "five":
-    case "six":
-    case "seven":
-    case "eight":
-    case "nine":
-    case "decimal":
-    case "zero":
-      if(flag === 0){
-        num1 = num1 + `${numValue}`;
-      }
-      else{
-        num2 = num2 + `${numValue}`;
-      }
+      numArr = [];
+      operator= "";
+      i = 0;
       break;
     case "divide":
-      operator = "/";
-      flag = 1;
+      numArr[i++] = "/";
       break;
     case "mul":
-      operator = '*';
-      flag = 1;
+      numArr[i++] = "*";
       break;
     case "add":
-      operator = '+';
-      flag = 1;
+      numArr[i++] = "+";
       break;
     case "sub":
-      operator = '-';
-      flag = 1;
+      numArr[i++] = "-";
       break;
     case "equal":
-      populateDisplay(displayValue);
+      populateDisplay("AC");
+      populateDisplay(result(numArr));
+      return;
+    case "dot":
+      
       break;
     default:
-      console.log("Unknown button clicked!");
+      numArr[i++] = `${text}`;
+      break;
   }
+  populateDisplay(text);
 }
 
 
-
-function populateDisplay(variable){
-  if(variable === "DEL"){
-    output.textContent =  output.textContent.slice(0, -1);
-    return;
-  }
-  if(variable === "AC"){
-    output.textContent =  "";
-    return;
-  }
-  if(+(variable) >= 0 || +(variable) <= 9 || variable === "."){
-    if(flag){
-      output.textContent =  output.textContent + " " +`${variable}`;
+function result(arr){
+  let j = 0;
+  let num1 = "";
+  let num2 = "";
+  while(j < numArr.length){
+    if(math.includes(arr[j])){
+      if(operator){
+        num1 = operate(operator, num1, num2);
+        num2 = "";
+      }
+      operator = arr[j];
     }
     else{
-      output.textContent =  output.textContent + `${variable}`;
+      if(!operator){
+        num1 += arr[j];
+      }
+      else{
+        num2 += arr[j];
+      }
     }
-    return;
+    j++;
   }
-  output.textContent =  output.textContent + " " +`${variable}`;
+  return operate(operator, num1, num2);
+}
+
+function operate(operator, result, num){
+  result = +(result);
+  num = +(num);
+  switch (operator) {
+    case "+":
+      return result + num;
+    case "-":
+      return result - num;
+    case "*":
+      return result * num;
+    case "/":
+      return result / num;
+    default:
+      break;
+  }
+}
+
+function populateDisplay(text){
+  switch (text) {
+    case "DEL":
+      output.textContent = (output.textContent.at(-2) === " ") ? 
+         output.textContent.slice(0, -2) : 
+         output.textContent = output.textContent.slice(0, -1);
+      return;
+    case "AC":
+      output.textContent = "";
+      return;
+    case "+":
+    case "-":
+    case "×":
+    case "÷":
+      output.textContent = `${output.textContent} ${text} `;
+      return;
+    default:
+      output.textContent = output.textContent + text;
+      return;
+  }
+  
 }
 
 
